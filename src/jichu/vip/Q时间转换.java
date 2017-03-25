77ac5d4c05ee0a0c36080b1cd0b6b6a130884fed
Tom12345678q <1524334009@qq.com>
2017-03-25 21:51:51 +0800

    基础练习的最后一道题时间转换完成

--- src/jichu/vip/Q时间转换.java
+++ src/jichu/vip/Q时间转换.java
@@ -1,5 +1,21 @@
 package jichu.vip;
 
+import java.util.Scanner;
+
 public class Q时间转换 {
+	
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		
+		int h=0;//时
+		int m=0;//分
+		int s=0;//秒
+		
+		h=n/3600;
+		m=(n-h*3600)/60;
+		s=n-h*3600-m*60;
+		System.out.println(h+":"+m+":"+s);
+	}
 
 }
